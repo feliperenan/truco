36f547239194d1e9921b28b1e7289de0b24ec560
Felipe Renan <felipe.renan@heathmont.net>
2020-08-03 20:38:18 +0300

    Fix issue when setting the special card

--- apps/engine/lib/engine/card.ex
+++ apps/engine/lib/engine/card.ex
@@ -7,12 +7,22 @@ defmodule Engine.Card do
   @symbols_with_index Enum.with_index(@symbols)
 
   @doc """
+  Returns available symbols from weakest to strongest according to Truco rules.
+  """
+  def symbols, do: @symbols
+
+  @doc """
+  Returns valid suits for a card.
+  """
+  def suits, do: @suits
+
+  @doc """
   Creates a card according to the given suit and symbol once they are valid.
 
   ### Examples
 
       iex> Card.new(:hearts, "3")
-      %Card{suit: :hearts, symbol: "3"}
+      %Card{suit: :hearts, symbol: "3", strength: 9}
 
   """
   def new(suit, symbol) when suit in @suits and symbol in @symbols do
@@ -60,11 +70,39 @@ defmodule Engine.Card do
     end
   end
 
+  @doc """
+  Set the card special when the given card is one symbol higher than the faced up card. Usually,
+  we call this card as "manilha". This also change the card strength according to its suit:
+
+  * diamonds - 10
+  * spades   - 11
+  * hears    - 12
+  * clubs    - 13
+
+  ### Examples
+
+      iex> faced_up = Card.new(:spades, "7")
+      iex> :clubs |> Card.new("Q") |> Card.set_special(faced_up)
+      %Card{symbol: "Q", suit: :clubs, special: true, strength: 13}
+
+      iex> faced_up = Card.new(:clubs, "K")
+      iex> :hearts |> Card.new("A") |> Card.set_special(faced_up)
+      %Card{symbol: "A", suit: :hearts, special: true, strength: 12}
+
+  """
   def set_special(%__MODULE__{} = card, faced_up) do
     {_sym, card_pos} = get_card_position(card)
-    {_sym, faced_up_pos} = get_card_position(faced_up)
 
-    if card_pos + 1 == faced_up_pos do
+    # Because I'm relying on the symbol positions in the @symbols_with_index list, I need to return
+    # -1 when it is the last position since first symbol (4) will be special when last symbol (3)
+    # is faced up.
+    faced_up_pos =
+      case get_card_position(faced_up) do
+        {_sym, 9} -> -1
+        {_sym, faced_up_pos} -> faced_up_pos
+      end
+
+    if card_pos - 1 == faced_up_pos do
       %{card | special: true, strength: set_strength(card.suit)}
     else
       card
--- apps/engine/test/engine/card_test.exs
+++ apps/engine/test/engine/card_test.exs
@@ -4,4 +4,49 @@ defmodule Engine.CardTest do
   alias Engine.Card
 
   doctest Card
+
+  describe "set_special/2" do
+    for {symbol, position} <- Card.symbols() |> Enum.with_index() do
+      @symbol symbol
+      @position position
+      @previous_symbol Enum.at(Card.symbols(), @position - 1)
+
+      test "#{@symbol} is special when #{@previous_symbol} is faced up" do
+        faced_up =
+          Card.suits()
+          |> Enum.random()
+          |> Card.new(@previous_symbol)
+
+        card =
+          Card.suits()
+          |> Enum.random()
+          |> Card.new(@symbol)
+          |> Card.set_special(faced_up)
+
+        assert card.special == true
+        assert card.strength > 9
+      end
+    end
+
+    test "4 must be special when 3 is faced up" do
+      faced_up = Card.new(:spades, "3")
+
+      diamonds = Card.new(:diamonds, "4")
+      spades = Card.new(:spades, "4")
+      hearts = Card.new(:hearts, "4")
+      clubs = Card.new(:clubs, "4")
+
+      assert %Card{special: true, strength: 10} = Card.set_special(diamonds, faced_up)
+      assert %Card{special: true, strength: 11} = Card.set_special(spades, faced_up)
+      assert %Card{special: true, strength: 12} = Card.set_special(hearts, faced_up)
+      assert %Card{special: true, strength: 13} = Card.set_special(clubs, faced_up)
+    end
+
+    test "is not special when the given card is not one" do
+      card = Card.new(:diamonds, "3")
+      faced_up = Card.new(:spades, "4")
+
+      assert %Card{special: false} = Card.set_special(card, faced_up)
+    end
+  end
 end
